3ef3ea4927d4a99dbf91aee19e3119b3265f9884
escalopa <ahmad.helaly.dev@gmail.com>
2022-11-22 18:07:38 +0300

    Add Test for login & create

---
+++ src/test/java/org/example/basic/CreateWorkSpaceTest.java
@@ -0,0 +1,18 @@
+package org.example.basic;
+
+import org.example.Models.Workspace;
+import org.example.TestBase;
+import org.junit.Test;
+
+public class CreateWorkSpaceTest {
+
+    @Test
+    public static void run(TestBase testBase){
+        String workspaceName = "Selenuim workspace 000";
+        String workspaceSummary = "a workspace for selenuim 002";
+        Workspace workspace = new Workspace(workspaceName, workspaceSummary);
+
+        testBase.login();
+        testBase.createWorkspace(workspace);
+    }
+}
