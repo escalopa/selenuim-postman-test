509c93c6c021cce7159ea034dabcf5d92d3b1037
escalopa <ahmad.helaly.dev@gmail.com>
2022-11-22 18:06:56 +0300

    Add helper functions & Main classes

---
+++ src/main/java/org/example/Models/User.java
@@ -0,0 +1,12 @@
+package org.example.Models;
+
+public class User {
+
+    public String username;
+    public String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
---
+++ src/main/java/org/example/Models/Workspace.java
@@ -0,0 +1,13 @@
+package org.example.Models;
+
+public class Workspace {
+
+    public String name;
+    public String summary;
+
+    public Workspace(String name, String summary) {
+        this.name = name;
+        this.summary = summary;
+    }
+
+}
